7f8be440fc31bcd25abf979b172da19ef9d9591c
Chidz <aizencraze@gmail.com>
2025-08-14 12:19:11 +0100

    refac: made builder more generic

--- app/stop-and-search/[forceId]/[date]/stops/page.tsx
+++ app/stop-and-search/[forceId]/[date]/stops/page.tsx
@@ -1,4 +1,5 @@
-import { StopsTableDataBuilder } from "@/features/stops/lib/StopsTableDataBuilder";
+import { StopsDataBuilder } from "@/features/stops/lib/StopsTableDataBuilder";
+import { QualitativeStop, Stop } from "@/features/stops/lib/types";
 import { PoliceApiResponseDirector } from "@/lib/PoliceApiResponseDirector";
 
 interface StopsTablePageProps {
@@ -9,15 +10,26 @@ export default async function StopsTablePage({ params }: StopsTablePageProps) {
 	const { forceId, date } = await params;
 
 	const stopsTablePageApiDirector = new PoliceApiResponseDirector(
-		new StopsTableDataBuilder(forceId, date)
+		new StopsDataBuilder(forceId, date, [
+			"outcome",
+			"self_defined_ethnicity",
+			"gender",
+			"outcome_linked_to_object_of_search",
+			"removal_of_more_than_outer_clothing",
+			"outcome_object",
+			"operation_name",
+			"involved_person",
+		])
 	);
 
 	const { data } = await stopsTablePageApiDirector.constructApiResponse(); // TODO: destructor error
 
+	const qualitativeStopData = data as QualitativeStop[];
+
 	return (
 		<>
 			<h1>Welcome to the stops page</h1>
-			<pre>{JSON.stringify(data, null, 4)}</pre>
+			<pre>{JSON.stringify(qualitativeStopData, null, 4)}</pre>
 		</>
 	);
 }
--- features/stops/utils/index.ts
+++ features/stops/utils/index.ts
@@ -1,6 +1,15 @@
 import { PoliceApiResponseTypes } from "@/lib/types";
-import { Stop } from "../lib/types";
+import { Stop, StopKeys } from "../lib/types";
 
 export function isStopsApiData(data: PoliceApiResponseTypes): data is Stop[] {
 	return data.length > 0 && "age_range" in data[0];
+}
+
+export function pickSpecificFieldsFromAStop<F extends StopKeys>(
+	stopObj: Stop,
+	fields: F[]
+) {
+	return Object.fromEntries(
+		fields.map((field) => [field, stopObj[field]])
+	) as Pick<Stop, F>;
 }
